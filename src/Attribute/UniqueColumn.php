2ca067f748ad8555fe6613ec5caf09e8c15020cc
tourze-ci <you@example.com>
2025-03-28 06:07:58 +0000

    feat: 添加 symfony-utm-bundle 依赖

---
+++ src/Attribute/UniqueColumn.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DoctrineEnhanceBundle\Attribute;
+
+/**
+ * 标记指定实体的成员关联一个唯一索引
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
+class UniqueColumn
+{
+    public function __construct(
+        public readonly string $group = 'group1',
+    ) {
+    }
+}
