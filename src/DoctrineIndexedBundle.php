7474ecfb534622fca846c82b3cbc64f59d691a33
github-ci <you@example.com>
2025-05-25 08:22:12 +0000

    update

--- src/DoctrineIndexedBundle.php
+++ src/DoctrineIndexedBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\DoctrineIndexedBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class DoctrineIndexedBundle extends Bundle
+class DoctrineIndexedBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+        ];
+    }
 }
